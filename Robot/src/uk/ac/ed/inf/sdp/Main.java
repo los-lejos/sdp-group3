9bc35561ece246d1b0367504f0870af445545679
Andrew Johnston <a.n.johnston@sms.ed.ac.uk>
2014-01-16 20:36:14 +0000

    avoid using default package

---
+++ Robot/src/uk/ac/ed/inf/sdp/Main.java
@@ -0,0 +1,21 @@
+package uk.ac.ed.inf.sdp;
+import lejos.nxt.*;
+
+public class Main {
+	public static void main (String[] args) {
+		LightSensor light = new LightSensor(SensorPort.S1);
+		light.calibrateLow();
+		
+		Motor.A.setSpeed(720);
+		Motor.C.setSpeed(720);
+	  	Motor.A.forward();
+		Motor.C.forward();
+
+		while(Button.readButtons() == 0 && 
+				light.getLightValue() < 30);
+		
+		Motor.A.stop();
+		Motor.C.stop();
+		Button.waitForAnyPress();
+	}
+}
