58ce71fc6c5b11e4844a02a60f92c3a0ad1e5ea0
Craig Wilkinson <git@craigwilkinson.co.uk>
2014-01-17 15:04:59 +0000

    Now we have a simpler system; a game loop.

--- Milestone_1/src/Main.java
+++ Milestone_1/src/Main.java
@@ -1,5 +1,7 @@
 import lejos.nxt.*;
 
+// remember that reverse is forward and vice versa due
+// to the motor orientations!
 public class Main {
 	
 	public enum State {
@@ -8,9 +10,9 @@ public class Main {
 	
     public static final NXTRegulatedMotor leftMotor = Motor.A;
     public static final NXTRegulatedMotor rightMotor = Motor.C;
-    public static State currentState = State.FORWARD;
     
     public static long lastSensor = 0;
+    public static State currentState = State.FORWARD;
     
     private static final int LightCutoff = 40;
     private static final int RobotMoveSpeed = 720;
@@ -19,103 +21,62 @@ public class Main {
     
 	
 	public static void main (String[] args) {
-		LightSensor lightL = new LightSensor(SensorPort.S1);
-        LightSensor lightR = new LightSensor(SensorPort.S4);
+		LightSensor leftLight = new LightSensor(SensorPort.S1);
+        LightSensor rightLight = new LightSensor(SensorPort.S4);
         
 		System.out.println("Press a button for a great time!");
         Button.waitForAnyPress();
 
-		rightMotor.setSpeed(RobotMoveSpeed);
-		leftMotor.setSpeed(RobotMoveSpeed);
-	  	leftMotor.backward();
-		rightMotor.backward();
+        // start moving to begin with
+        rightMotor.setSpeed(RobotMoveSpeed);
+        leftMotor.setSpeed(RobotMoveSpeed);
+        leftMotor.backward();
+        rightMotor.backward();
 
-		while(Button.readButtons() == 0) {
-			System.out.println(lightL.getLightValue());
-			// Keep going forward until we see white.
-			while(Button.readButtons() == 0 &&
-                    lightL.getLightValue() < LightCutoff &&
-                    lightR.getLightValue() < LightCutoff)
-			{
-				System.out.println(lightL.getLightValue());
-			}
-			
-			// Turn away from white.
-			// when detecting with left sensor, turn right
-			if (lightL.getLightValue() >= LightCutoff) {
-				// if both sensors detected white at roughly the same time
-				if (lightR.getLightValue() >= LightCutoff) {
-					System.out.println("reversing");
-					
-					// reverse for a period, not ideal
-					leftMotor.forward();
-					rightMotor.forward();
-					
-					long timeNow = System.currentTimeMillis();
-					while (System.currentTimeMillis() < timeNow + ReverseTime);
-					
-					leftMotor.backward();
-					rightMotor.backward();
-				} else {
-					System.out.println("Turning right");
+        // begin main loop
+        boolean running = true;
+        while (running) {
 
-					lastSensor = System.currentTimeMillis();
+            // System.out.println(leftLight.getLightValue());
+            
+            if (currentState == State.FORWARD) {
 
-					currentState = State.TURNING_RIGHT;
-					rightMotor.setSpeed(RobotTurnSpeed);
-					leftMotor.setSpeed(RobotTurnSpeed);
-					rightMotor.forward();
-					
-					// Keep turning until no white under relevant sensor.
-					while(Button.readButtons() == 0 &&
-		                    currentState == State.TURNING_RIGHT) {
-						System.out.println(lightL.getLightValue());
-						if (lightL.getLightValue() < LightCutoff) {
-							rightMotor.setSpeed(RobotMoveSpeed);
-							leftMotor.setSpeed(RobotMoveSpeed);
-							rightMotor.backward();
-							currentState = State.FORWARD;
-						}
-					}
-				}
-			} else { // when detecting with left sensor, turn right
-				// if both sensors detected white at roughly the same time
-				if (lightL.getLightValue() >= LightCutoff) {
-					System.out.println("reversing");
-					
-					// reverse for a period, not ideal
-					leftMotor.forward();
-					rightMotor.forward();
-					
-					long timeNow = System.currentTimeMillis();
-					while (System.currentTimeMillis() < timeNow + ReverseTime);
-					
-					leftMotor.backward();
-					rightMotor.backward();
-				} else {
-					System.out.println("Turning left");
+                if (leftLight.getLightValue() >= LightCutoff) {
+                    System.out.println("Turning right.");
+                    currentState == State.TURNING_RIGHT;
+                    rightMotor.forward();
+                } else if (rightLight.getLightValue() >= LightCutoff) {
+                    System.out.println("Turning left.");
+                    currentState == State.TURNING_LEFT;
+                    leftMotor.forward();
+                }
 
-					lastSensor = System.currentTimeMillis();
+            } else if (currentState == State.TURNING_RIGHT) {
+                
+                if (leftLight.getLightValue() < LightCutoff) {
+                    System.out.println("Going forward.");
+                    currentState == State.FORWARD;
+                    rightMotor.backward();
+                }
 
-					currentState = State.TURNING_LEFT;
-					rightMotor.setSpeed(RobotTurnSpeed);
-					leftMotor.setSpeed(RobotTurnSpeed);
-					leftMotor.forward();
-					
-					// Keep turning until no white under relevant sensor.
-					while(Button.readButtons() == 0 &&
-		                    currentState == State.TURNING_LEFT) {
-						System.out.println(lightR.getLightValue());
-						if (lightR.getLightValue() < LightCutoff) {		
-							rightMotor.setSpeed(RobotMoveSpeed);
-							leftMotor.setSpeed(RobotMoveSpeed);
-							leftMotor.backward();
-							currentState = State.FORWARD;
-						}
-					}
-				}
-			}
-		}
+            } else if (currentState == State.TURNING_LEFT) {
+
+                if (rightLight.getLightValue() < LightCutoff) {
+                    System.out.println("Going forward.");
+                    currentState == State.FORWARD;
+                    leftMotor.backward();
+                }
+
+            } else
+                System.out.println("Impossible state!");
+
+
+            if (Button.readButtons() != 0)
+                running = false;
+
+        }
+
+
 		
         Button.waitForAnyPress();
 	}
