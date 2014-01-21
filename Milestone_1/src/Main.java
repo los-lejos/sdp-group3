73ebc424172c948ef444b3f6bbb29a7583e3c265
Joris Urbaitis <j.s.urbaitis@gmail.com>
2014-01-21 12:51:15 +0000

    Changing Milestone1 to use DifferentialPilot instead of our custom motor controls

--- Milestone_1/src/Main.java
+++ Milestone_1/src/Main.java
@@ -1,4 +1,5 @@
 import lejos.nxt.*;
+import lejos.robotics.navigation.DifferentialPilot;
 
 public class Main {
 	
@@ -9,35 +10,37 @@ public class Main {
     public static final NXTRegulatedMotor leftMotor = Motor.A;
     public static final NXTRegulatedMotor rightMotor = Motor.C;
     
-    public static long lastSensor = 0;
     public static State currentState = State.FORWARD;
     
     private static final int LightCutoff = 40;
-    private static final int RobotMoveSpeed = 200;
-    private static final int RobotTurnSpeed = 50;
-
+    private static final double RobotMoveSpeed = 120;
+    private static final double RobotTurnSpeed = 15;
+    
+    private static final int TireDiameterMm = 56;
+    private static final int TrackWidthMm = 100;
+    
 	public static void main (String[] args) {
 		LightSensor leftLight = new LightSensor(SensorPort.S1);
         LightSensor rightLight = new LightSensor(SensorPort.S4);
-
-        // start moving to begin with
-        rightMotor.setSpeed(RobotMoveSpeed);
-        leftMotor.setSpeed(RobotMoveSpeed);
-        leftMotor.backward();
-        rightMotor.backward();
         
-		  // Busy wait for a sensor to hit the white ground
-		  while(leftLight.getLightValue() < LightCutoff &&
-		          rightLight.getLightValue() < LightCutoff);
+        DifferentialPilot pilot = new DifferentialPilot(TireDiameterMm, TrackWidthMm, leftMotor, rightMotor, true);
+        pilot.setTravelSpeed(RobotMoveSpeed);
+        pilot.setRotateSpeed(RobotTurnSpeed);
+        
+        // start moving to begin with
+        pilot.forward();
 
-		  State greenDirection;
-		  
-		  if(leftLight.getLightValue() >= LightCutoff) {
-		  	greenDirection = State.TURNING_RIGHT;
-		  }
-		  else {
+		// Busy wait for a sensor to hit the white ground
+		while(leftLight.getLightValue() < LightCutoff &&
+		      rightLight.getLightValue() < LightCutoff);
+
+		State greenDirection;
+		if(leftLight.getLightValue() >= LightCutoff) {
+			greenDirection = State.TURNING_RIGHT;
+		}
+		else {
 		  	greenDirection = State.TURNING_LEFT;
-		  }
+		}
 
         // begin main loop
         boolean running = true;
@@ -47,68 +50,41 @@ public class Main {
         		if(greenDirection == State.TURNING_LEFT) {
         			System.out.println("Turning left.");
                     currentState = State.TURNING_LEFT;
-                    rightMotor.setSpeed(RobotTurnSpeed);
-                    leftMotor.setSpeed(RobotTurnSpeed);
-                    leftMotor.forward();	
-                    rightMotor.backward();
+                    pilot.rotateLeft();
         		} else {
         			System.out.println("Turning right.");
                     currentState = State.TURNING_RIGHT;
-                    rightMotor.setSpeed(RobotTurnSpeed);
-                    leftMotor.setSpeed(RobotTurnSpeed);
-                    rightMotor.forward();
-                    leftMotor.backward();
+                    pilot.rotateRight();
         		}
         	}
             
             if (currentState == State.FORWARD) {
-
                 if (leftLight.getLightValue() >= LightCutoff) {
                     System.out.println("Turning right.");
                     currentState = State.TURNING_RIGHT;
-                    rightMotor.setSpeed(RobotTurnSpeed);
-                    leftMotor.setSpeed(RobotTurnSpeed);
-                    rightMotor.forward();
+                    pilot.rotateRight();
                 } else if (rightLight.getLightValue() >= LightCutoff) {
                     System.out.println("Turning left.");
                     currentState = State.TURNING_LEFT;
-                    rightMotor.setSpeed(RobotTurnSpeed);
-                    leftMotor.setSpeed(RobotTurnSpeed);
-                    leftMotor.forward();
+                    pilot.rotateLeft();
                 }
-
-            } else if (currentState == State.TURNING_RIGHT) {
-                
+            } else if (currentState == State.TURNING_RIGHT || currentState == State.TURNING_LEFT) {
                 if (leftLight.getLightValue() < LightCutoff &&
                     rightLight.getLightValue() < LightCutoff) {
                     System.out.println("Going forward.");
                     currentState = State.FORWARD;
-                    rightMotor.setSpeed(RobotMoveSpeed);
-                    leftMotor.setSpeed(RobotMoveSpeed);
-                    rightMotor.backward();
+                    pilot.forward();
                 }
-
-            } else if (currentState == State.TURNING_LEFT) {
-
-                if (rightLight.getLightValue() < LightCutoff &&
-                	leftLight.getLightValue() < LightCutoff) {
-                    System.out.println("Going forward.");
-                    currentState = State.FORWARD;
-                    rightMotor.setSpeed(RobotMoveSpeed);
-                    leftMotor.setSpeed(RobotMoveSpeed);
-                    leftMotor.backward();
-                }
-
-            } else
+            } else {
                 System.out.println("Impossible state!");
+            }
 
 
             if (Button.readButtons() != 0)
                 running = false;
-
         }
 
-
+		pilot.stop();
 		
         Button.waitForAnyPress();
 	}
