cdff0c0b01baceea9d07cd70352575983dee4afa
cycleseven <switchtransition@gmail.com>
2014-01-23 15:50:09 +0000

    Can use different values for rotate/travel speed now.

---
+++ Milestone_1/src/PilotTesting.java
@@ -0,0 +1,44 @@
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.robotics.navigation.DifferentialPilot;
+
+public class PilotTesting {
+	
+	public enum State {
+		FORWARD, TURNING_RIGHT, TURNING_LEFT
+	}
+	
+    public static final NXTRegulatedMotor leftMotor = Motor.A;
+    public static final NXTRegulatedMotor rightMotor = Motor.C;
+    
+    public static State currentState = State.FORWARD;
+    
+    private static double RobotMoveSpeed;
+    private static double RobotTurnSpeed;
+    
+    private static final int TireDiameterMm = 56;
+    
+    private static final int TrackWidthMm = 114;
+    
+	public static void main (String[] args) {
+
+        DifferentialPilot pilot = new DifferentialPilot(TireDiameterMm, TrackWidthMm, leftMotor, rightMotor, true);
+        
+        RobotMoveSpeed = pilot.getMaxTravelSpeed() * 0.6;
+        RobotTurnSpeed = pilot.getMaxRotateSpeed() * 0.1;
+        pilot.setTravelSpeed(RobotMoveSpeed);
+        pilot.setRotateSpeed(RobotTurnSpeed);
+
+        for (int i = 0; i < 4; i++) {
+        	pilot.travel(50);
+        	long beforeTime = System.currentTimeMillis();
+        	while (System.currentTimeMillis() - beforeTime < 1500)
+        		pilot.rotateLeft();
+        }
+        
+        pilot.stop();
+        Button.waitForAnyPress();
+	}
+
+}
