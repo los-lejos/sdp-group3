db26ef70c6c039f421e8805b95cae9460a23c24a
Joris Urbaitis <j.s.urbaitis@gmail.com>
2014-01-27 18:10:30 +0000

    Adding a test for sending an instruction over bluetooth

--- Dice/src/dice/Main.java
+++ Dice/src/dice/Main.java
@@ -3,6 +3,9 @@ package dice;
 import java.io.IOException;
 
 import dice.communication.RobotCommunication;
+import dice.communication.RobotCommunicationCallback;
+import dice.communication.RobotInstruction;
+import dice.communication.RobotType;
 
 /*
  * @author Joris S. Urbaitis
@@ -12,6 +15,26 @@ public class Main {
 	public static void main (String[] args) {
 		RobotCommunication.getInstance().init();
 		
+		RobotInstruction test = new RobotInstruction(
+			RobotInstruction.MOVE_TO, (byte)4, (byte)4, RobotType.ATTACKER,
+			new RobotCommunicationCallback() {
+				@Override
+				public void onError() {
+					System.out.println("Error");
+				}
+
+				@Override
+				public void onTimeout() {
+					System.out.println("Timeout");
+				}
+
+				@Override
+				public void onDone() {
+					System.out.println("Done");
+				}
+		});
+
+		RobotCommunication.getInstance().sendInstruction(test);
 
 		try {
 			System.in.read();
