e12443ee7addad154d9ba2294b9812ce9518109e
Joris Urbaitis <j.s.urbaitis@gmail.com>
2014-01-27 16:56:52 +0000

    Fixing a bug were the handshake was not being verified properly and was causing errors

--- Dice/src/dice/Main.java
+++ Dice/src/dice/Main.java
@@ -1,5 +1,7 @@
 package dice;
 
+import java.io.IOException;
+
 import dice.communication.BluetoothCommunicationException;
 import dice.communication.BluetoothRobotConnection;
 
@@ -9,16 +11,28 @@ import dice.communication.BluetoothRobotConnection;
 
 public class Main {
 	public static void main (String[] args) {
-		BluetoothRobotConnection conn = new BluetoothRobotConnection("OptimusPrime", "macaddr");
+		BluetoothRobotConnection conn = new BluetoothRobotConnection("OptimusPrime", "0016530A553F");
+		//BluetoothRobotConnection conn = new BluetoothRobotConnection("Ball-E", "0016530A5C22");
 		try {
+			System.out.println("Opening Bluetooth connection");
 			conn.openConnection();
+			System.out.println("Performing handshake");
 			conn.handshake();
 		} catch (BluetoothCommunicationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		conn.closeConnection();
+		
 		// Start the communications thread
-		conn.start();
+		//conn.start();
 	}
 }
