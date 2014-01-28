aa0fd8c1bac3c9af3053666f2727181e445b9517
Joris Urbaitis <j.s.urbaitis@gmail.com>
2014-01-28 17:34:28 +0000

    Adding proper bluetooth code structure for the robot that will handle the handshake, incoming instructions and sending messages to Dice

---
+++ Robot/src/robot/communication/BluetoothCommunicationException.java
@@ -0,0 +1,14 @@
+package robot.communication;
+
+/*
+ * @author Joris S. Urbaitis
+ */
+
+public class BluetoothCommunicationException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public BluetoothCommunicationException(String msg) {
+		super(msg);
+	}
+}
