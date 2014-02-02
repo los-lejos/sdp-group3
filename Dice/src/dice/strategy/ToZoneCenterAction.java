882ee690494fbbe3939cadb514ea481fd89460af
Sam Stern <samuelostern@gmail.com>
2014-02-02 16:37:27 +0000

    ShootAction and ToGoalAction

--- Dice/src/dice/strategy/ToZoneCenterAction.java
+++ Dice/src/dice/strategy/ToZoneCenterAction.java
@@ -10,27 +10,47 @@ import dice.state.WorldState;
 
 public class ToZoneCenterAction extends StrategyAction {
 	
-
+	byte xD,yD,xA,yA; // TODO make -A and -B the zone centers for the attacking and defending robots
+	
 	public ToZoneCenterAction(RobotType target) {
 		super(target);
 	}
 
 	@Override
 	public boolean isPossible(WorldState state) {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	protected int calculateUtility(WorldState state) {
-		// TODO Auto-generated method stub
-		return 0;
+		switch( this.getTargetRobot()) {
+		case ATTACKER:
+			if (state.getOpponentAttacker().getHasBall()||state.getOpponentDefender().getHasBall()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		case DEFENDER:
+			if (state.getOpponentAttacker().getHasBall()|| state)
+			
+		}
+		
+//		if (state.getOpponentAttacker().getHasBall() && this.getTargetRobot().equals(state.getOurAttacker())) {
+//			return 2;
+//		} else if (state.getOpponentAttacker().getHasBall() && this.getTargetRobot.equals(state.getOurDefender())) {
+//			return 0;
+//		} else if (this.getTargetRobot().equals(state.getOurAttacker())) {
+//			return 0;
+//		} else {
+//			return 1;
+//		}
 	}
 
 	@Override
-	public RobotInstruction getInstruction() {
-		// TODO Auto-generated method stub
-		return null;
+	public RobotInstruction getInstruction(WorldState state) {
+		if (this.getTargetRobot().equals(other)) {
+			
+		}
 	}
 
 }
