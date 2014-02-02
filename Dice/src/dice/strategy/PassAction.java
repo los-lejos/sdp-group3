3b09c0779999b1579098bf3205f609b648dec145
Andrew Johnston <a.n.johnston@sms.ed.ac.uk>
2014-02-02 16:20:28 +0000

    Remove Milestone_1

--- Dice/src/dice/strategy/PassAction.java
+++ Dice/src/dice/strategy/PassAction.java
@@ -1,9 +1,16 @@
 package dice.strategy;
 
+import org.jfree.util.Log;
+
 import shared.RobotInstructions;
 import dice.communication.RobotInstruction;
 import dice.communication.RobotType;
 import dice.state.WorldState;
+
+/**
+ *
+ * @author Andrew Johnston
+ */
 
 public class PassAction extends StrategyAction {
 
@@ -13,17 +20,44 @@ public class PassAction extends StrategyAction {
 	
 	@Override
 	public boolean isPossible(WorldState state) {
-	
-		return false;
+		switch (target) {
+		case ATTACKER:
+			// do we need to pass to the goalie?
+			// probably not
+			return false;
+		case DEFENDER:
+			// we need to have the ball
+			if (state.getOurDefender().getHasBall() == true) return true;
+			else return false;
+		default:
+			// something went wrong
+			Log.debug("The sky is falling");
+			return false;
+		}
 	}
 
 	@Override
 	protected int calculateUtility(WorldState state) {
-		return 0;
+		switch (target) {
+		case ATTACKER:
+			return 0;
+		case DEFENDER:
+			// arbitrary
+			return 1;
+		default:
+			return 0;
+		}
 	}
 
 	@Override
 	public RobotInstruction getInstruction(WorldState state) {
+		
+		switch (target) {
+		case ATTACKER:
+		case DEFENDER:		
+		}
+		
+		
 		return null;
 	}
 
