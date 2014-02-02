5cd090a0fc46c4ee3311016aeeb1e1b9ec762088
spiderpete <g.i.joe_soldier@abv.bg>
2014-02-02 13:20:42 +0000

    Completion of some methods and discussion of some comments

--- Robot/src/robot/navigation/HolonomicPilot.java
+++ Robot/src/robot/navigation/HolonomicPilot.java
@@ -13,22 +13,29 @@ import lejos.robotics.navigation.MoveListener;
  * This class gives us something similar to DifferentialPilot, but
  * for the holonomic design.
  * 
- * TODO Is trackWidth relevant? Need to figure out odometry stuff for this design.
+ *  Is trackWidth relevant? Need to figure out odometry stuff for this design.
  * 		Can't just steal this directly from DifferentialPilot since angle info
  * 		depends on differences in wheel turns.
+ *  - I cannot find any use of it.
+ *  
  * TODO Implementing MoveController (needed if we want to use Navigator) forces
  * 		is into implementing some methods that IMO don't make much sense given
  * 		the robot design:
  * 			eg. travel(distance) (since we strafe, just travelling
  * 				forwards/backwards by a set amount is pretty unhelpful).
- * TODO Actually finish writing this class
+ * 		- What if we calculate the distance from a = current location, to b = goal/aiming location,
+ * 		this way we can travel the distance = |b - a|... maybe it will be helpful in this scenario
+ * 
+ *  Actually finish writing this class
+ *  - TODO Done?
  */
 
 public class HolonomicPilot implements MoveController {
 	
 	private double wheelDiameter;
 	private double trackWidth;
-	private double travelSpeed;
+	private double[] travelSpeed; // we can store the speed of the forwardMotor at position 0 and lateralMotor at position 1
+	private double acceleration; // there is only one variable because it is only sensible to add acceleration to the back/forward movement
 	private RegulatedMotor forwardMotor;
 	private RegulatedMotor lateralMotor;
 	private Move currentMovement;
@@ -96,8 +103,8 @@ public class HolonomicPilot implements MoveController {
 
 	@Override
 	public void travel(double distance, boolean immediateReturn) {
-		// TODO do some forward/backward travelling here
-		if (!immediateReturn) waitComplete();		
+		if (!immediateReturn) waitComplete();	
+		this.travel(distance); // Do some forward/backward travelling here
 	}
 	
 	// Travel a given amount in a given direction.
@@ -105,24 +112,33 @@ public class HolonomicPilot implements MoveController {
 	// Or x, y, heading could come from PC, if we choose to do it that way.
 	public void travel(double distance, double heading, boolean immediateReturn) {
 		if (!immediateReturn) waitComplete();
+		this.travel(distance);
+		lateralMotor.rotateTo(heading);
 	}
 
-	// TODO This sucks since we would probably like to set lateral speed independently
+	// This sucks since we would probably like to set lateral speed independently
 	// from forward/backward speed. Included since (right now, anyway) we need this
 	// if we want to implement MoveController. (Do we? We could write a custom navigator
 	// that takes in a HolonomicPilot rather than a MoveController...)
+	
+	// Resolved: We could have two variables passing the speed for the forwardMotor and lateralMotor
 	@Override
-	public void setTravelSpeed(double speed) {
-		forwardMotor.setSpeed((int) speed);
-		lateralMotor.setSpeed((int) speed); 
-		this.travelSpeed = speed;
+	public void setTravelSpeed(double speedF, double speedL) {
+		forwardMotor.setSpeed((int) speedF);
+		lateralMotor.setSpeed((int) speedL); 
+		this.travelSpeed[0] = speedF;
+		this.travelSpeed[1] = speedL;
 	}
 	
-	// TODO setAccelerationSpeed might be a nice/useful method.
+	// setAccelerationSpeed might be a nice/useful method.
 	//		(Seriously, we should do some acceleration. It will look so cool, guys.)
+	@Override
+	public void setAccelerationSpeed(double acceleration){
+		this.acceleration = acceleration;
+	}
 
 	@Override
-	public double getTravelSpeed() {
+	public double[] getTravelSpeed() {
 		return travelSpeed;
 	}
 
