376b80b798ac8454e59856681880f824479e130f
Andrew Johnston <a.n.johnston@sms.ed.ac.uk>
2014-02-03 19:41:06 +0000

    rv some more bad commits

--- Dice/src/dice/communication/RobotInstruction.java
+++ Dice/src/dice/communication/RobotInstruction.java
@@ -12,7 +12,6 @@ public class RobotInstruction {
 	private byte[] instruction;
 	private RobotCommunicationCallback callback;
 	
-	
 	public RobotInstruction(byte instructionType, byte param1, byte param2, RobotType robotType, RobotCommunicationCallback callback) {
 		this.robotType = robotType;
 		this.callback = callback;
