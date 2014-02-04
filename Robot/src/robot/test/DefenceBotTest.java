44776c4e0333162f40373b1cece4408edda271a6
spiderpete <g.i.joe_soldier@abv.bg>
2014-02-04 10:45:54 +0000

    Indentation fix

--- Robot/src/robot/test/DefenceBotTest.java
+++ Robot/src/robot/test/DefenceBotTest.java
@@ -3,40 +3,40 @@ import robot.navigation.HolonomicPilot;
 import lejos.nxt.*;
 
 public class DefenceBotTest {
-      public static void main (String[] args) throws InterruptedException {
- 
-    	  HolonomicPilot pilot = new HolonomicPilot(48, Motor.A, Motor.B);
+	public static void main (String[] args) throws InterruptedException {
+
+		HolonomicPilot pilot = new HolonomicPilot(48, Motor.A, Motor.B);
     	  
-    	  pilot.setTravelSpeed(200, 200);
-		  		  
-		  pilot.forward();
-		  Thread.sleep(5000);
-		  pilot.stop();
-		  
-		  pilot.right();
-		  Thread.sleep(5000);
-		  pilot.stop();   
-		  
-		  pilot.backward();
-		  Thread.sleep(5000);
-		  pilot.stop();
-		  
-		  pilot.left();
-		  Thread.sleep(5000);
-		  pilot.stop();
-	
-		  
-		  pilot.travel(0, 360, true);
-		  System.out.println(Motor.A.getTachoCount());
-		  System.out.println(Motor.B.getTachoCount());
-		  System.out.println();
+		pilot.setTravelSpeed(200, 200);
     	  
-		  pilot.travel(250, 45, true);
-		  System.out.println(Motor.A.getTachoCount());
-		  System.out.println(Motor.B.getTachoCount());
-		  System.out.println();
-		  
-    	  Button.waitForAnyPress();
+		pilot.forward();
+		Thread.sleep(5000);
+		pilot.stop();
+    	  
+		pilot.right();
+		Thread.sleep(5000);
+		pilot.stop();   
+  
+		pilot.backward();
+		Thread.sleep(5000);
+		pilot.stop();
+  
+		pilot.left();
+		Thread.sleep(5000);
+		pilot.stop();
+  
+
+		pilot.travel(0, 360, true);
+		System.out.println(Motor.A.getTachoCount());
+		System.out.println(Motor.B.getTachoCount());
+		System.out.println();
+  
+		pilot.travel(250, 45, true);
+		System.out.println(Motor.A.getTachoCount());
+		System.out.println(Motor.B.getTachoCount());
+		System.out.println();
+  
+		Button.waitForAnyPress();
 
       }
-}                                                             
+}
