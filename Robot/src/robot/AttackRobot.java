c01167181cffc6b65027d304e56ecfa9ef7b51a6
cycleseven <switchtransition@gmail.com>
2014-02-05 14:36:37 +0000

    Non-navigator MOVE_TO for attacker.

--- Robot/src/robot/AttackRobot.java
+++ Robot/src/robot/AttackRobot.java
@@ -5,10 +5,7 @@ import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
-import lejos.robotics.localization.OdometryPoseProvider;
 import lejos.robotics.navigation.DifferentialPilot;
-import lejos.robotics.navigation.Navigator;
-import lejos.robotics.navigation.Pose;
 
 /*
  * @author Owen Gillespie
@@ -26,50 +23,23 @@ public class AttackRobot extends Robot {
 	private static final NXTRegulatedMotor leftMotor = Motor.C;
 	private static final NXTRegulatedMotor rightMotor = Motor.A;
 	private final DifferentialPilot pilot;
-	private final OdometryPoseProvider poseProvider;
-	private final Navigator navigator;
 	
 	public AttackRobot() {
 		super(leftLightSensor, rightLightSensor, ballSensor);
 		pilot = new DifferentialPilot(tireDiameterMm, trackWidthMm, leftMotor, rightMotor, true);
-		poseProvider = new OdometryPoseProvider(pilot);
-		navigator = new Navigator(pilot, poseProvider);
 		kickMotor.setSpeed(kickSpeed);
 	}
 
 	@Override
 	protected void moveTo(int heading, int distance) {
-		poseProvider.setPose(new Pose()); // Reset to origin + 0 heading
-		int x, y;
-
-		// Assuming heading is in range 0-359.
-		if (heading == 0) {
-			x = distance;
-			y = 0;
-		} else if (heading > 0 && heading < 90) {
-			x = (int) (Math.sin(heading) * distance);
-			y = (int) (Math.cos(heading) * distance);
-		} else if (heading == 90) {
-			x = 0;
-			y = heading;
-		} else if (heading > 90 && heading < 180) {
-			x = - (int) (Math.sin(heading) * distance);
-			y = (int) (Math.cos(heading) * distance);
-		} else if (heading == 180) {
-			x = -distance;
-			y = 0;
-		} else if (heading > 180 && heading < 270) {
-			x = - (int) (Math.cos(heading) * distance);
-			y = - (int) (Math.sin(heading) * distance);
-		} else if (heading == 270) {
-			x = 0;
-			y = -distance;
+		if (heading >= 0 && heading < 180) {
+			pilot.rotate(heading);
+		} else if (heading >= 180 && heading < 360) {
+			pilot.rotate(heading - 360);
 		} else {
-			x = - (int) (Math.sin(heading) * distance);
-			y = - (int) (Math.cos(heading) * distance);
+			System.out.println("Bad heading value.");
 		}
-
-		navigator.goTo(x, y);
+		pilot.travel((float) distance / 10.0);
 	}
 
 	@Override
