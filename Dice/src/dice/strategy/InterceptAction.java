f640e27bb0c611477014be52c3cdba6d9af39cd0
Sam Stern <samuelostern@gmail.com>
2014-02-07 17:47:45 +0000

    BlockAction renamed to Intercept Action and 'new' BlockAction implemented

--- Dice/src/dice/strategy/InterceptAction.java
+++ Dice/src/dice/strategy/InterceptAction.java
@@ -1,37 +1,52 @@
 package dice.strategy;
 
-import shared.RobotInstructions;
 import dice.communication.RobotInstruction;
 import dice.communication.RobotType;
+import dice.state.GameObject;
+import dice.state.Vector2;
 import dice.state.WorldState;
 
-/**
- * @author Joris S. Urbaitis
+/*
+ * @author Sam Stern
+ * 
+ * extrapolate position of ball and see if its possible to block ball. if yes, then move to block the ball
  */
-
 public class InterceptAction extends StrategyAction {
-
-	private byte x, y;
 	
+
 	public InterceptAction(RobotType targetRobot) {
 		super(targetRobot);
 	}
 
+	private Vector2 whereToBlock;
+
+
+	@Override
+	// TODO Move content of this method to calculateUtility
 	public boolean isPossible(WorldState state) {
 		return true;
 	}
-	
-	public int calculateUtility(WorldState state) {
-		return 0;
+
+	@Override
+	protected int calculateUtility(WorldState state) {
+		GameObject ball = state.getBall();
+//		whereToBlock = new Vector2(x,ball.projectPath().getY(x));
+		boolean canReachBall = StratMaths.canReach(whereToBlock, getTargetObject(state));
+		if (canReachBall && !StratMaths.willCollideWithBall(getTargetObject(state))) {
+			return 2;
+		} else if (canReachBall && StratMaths.willCollideWithBall(getTargetObject(state))){
+			return 0;
+		} else {
+			return 1;
+		}
 	}
 
 	@Override
 	public RobotInstruction getInstruction(WorldState state) {
-		return new RobotInstruction(
-				RobotInstructions.MOVE_TO,
-				this.x,
-				this.y,
-				(byte) 0,
+		return RobotInstruction.CreateMoveTo(
+				StratMaths.cartesianToPolarTheta(whereToBlock),
+				StratMaths.cartestanToPolarR(whereToBlock),
 				this.getCallback());
 	}
+
 }
