977d9fd863402520c8c31df07f378713792a1a23
cycleseven <switchtransition@gmail.com>
2014-02-07 16:12:18 +0000

    Notify DICE with ball possession info.

--- Shared/shared/RobotInstructions.java
+++ Shared/shared/RobotInstructions.java
@@ -4,7 +4,12 @@ public final class RobotInstructions {
 	
 	public static final byte LENGTH = 5;
 	
+	// To robot
 	public static final byte MOVE_TO = 0;
 	public static final byte KICK_TOWARD = 1;
+	
+	// To DICE
+	public static final byte CAUGHT_BALL = -2;
+	public static final byte RELEASED_BALL = -3;
 
 }
