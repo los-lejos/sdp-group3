f348c39c2e32118d8eab2d37c59dbe5d65bcc479
spiderpete <g.i.joe_soldier@abv.bg>
2014-02-09 22:40:45 +0000

    KickerTest containing a travel function + separate class for Multiplexer
    We have to be careful with the lateral motor because if the motor spins
    the wheel at full speed (100%), the robot does not go in a straight
    line.

--- Milestone/src/KickerTest.java
+++ Milestone/src/KickerTest.java
@@ -1,53 +1,43 @@
+import lejos.nxt.Button;
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTMotor;
 
 
 public class KickerTest {
+	
+	public static void travel(NXTMotor motor, int power, double distance, String direction) throws Exception{
+		motor.setPower(power); // between 0% and 100%
+		if (direction.equals("left")){
+			motor.forward();
+		} else if (direction.equals("right")){
+			motor.backward();
+		} else {
+			System.out.println("Unknown direction");
+		}				
+		Thread.sleep((int) (distance*1000/59.4)); // the speed of the robot (having considered its current weight) is 59.4cm/sec
+		motor.stop();
+	}
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws Exception {
 /*
-		RegulatedMotor kickMotor = Motor.A;
-		
+		RegulatedMotor kickMotor = Motor.A;		
 		
 //		kickMotor.setSpeed(kickMotor.getMaxSpeed());
 		kickMotor.setSpeed(100);
-		kickMotor.forward();
-		
+		kickMotor.forward();		
 		
 		Motor.C.setSpeed(200);
 		Motor.C.forward();
 		Thread.sleep(1000);
 		Motor.C.stop();
 */
-		NXTMotor mamkamu = new NXTMotor(MotorPort.B);
+
+		NXTMotor lateralMotor = new NXTMotor(MotorPort.C);
+		travel (lateralMotor, 100, 14.85, "left");
+
 		
-		mamkamu.setPower(200);
-		mamkamu.backward();
-		Thread.sleep(1000);
+		Button.waitForAnyPress();
 		
-		//Button.waitForAnyPress();
-		
-		// IC2 Motor test
-		// DO NOT SEND 0x03 STATE TO ANY REGISTER!
-//		I2CPort I2Cport; //Create a I2C port
-//		I2Cport = SensorPort.S4; //Assign port
-//		I2Cport.i2cEnable(I2CPort.STANDARD_MODE);
-//		//Initialize port in standard mode
-//		I2CSensor I2Csensor = new I2CSensor(I2Cport);
-//		//Creates an I2CSensor
-//
-//		byte forward = (byte)1;
-//		byte backward = (byte)2;
-//		byte off = (byte)0;
-//		byte speed = (byte)255;
-//		I2Csensor.setAddress(0xB4);
-//		I2Csensor.sendData(0x01,backward);
-//		I2Csensor.sendData(0x02,speed);
-//		Thread.sleep(500);
-//		//I2Csensor.sendData(0x01,forward);
-//		//Thread.sleep(600);
-//		I2Csensor.sendData(0x01,off);
-//		I2Csensor.sendData(0x02,off);
 	}
 	
 }
