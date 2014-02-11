8852ba73bc4f58e7c52630fda4783636e8c3ae43
spiderpete <g.i.joe_soldier@abv.bg>
2014-02-11 17:00:23 +0000

    Rotating method fixed

--- Robot/src/robot/AttackRobot.java
+++ Robot/src/robot/AttackRobot.java
@@ -14,13 +14,13 @@ import lejos.robotics.navigation.DifferentialPilot;
 public class AttackRobot extends Robot {
 	
 	private static final int tireDiameterMm = 62;
-	private static final int trackWidthMm = 123;
+	private static final int trackWidthMm = 142;
 	private static final NXTRegulatedMotor kickMotor = Motor.B;
 	private static final LightSensor leftLightSensor = new LightSensor(SensorPort.S4);
 	private static final LightSensor rightLightSensor = new LightSensor(SensorPort.S1);
 	private static final UltrasonicSensor ballSensor = new UltrasonicSensor(SensorPort.S2);
-	private static final NXTRegulatedMotor leftMotor = Motor.A;
-	private static final NXTRegulatedMotor rightMotor = Motor.C;
+	private static final NXTRegulatedMotor leftMotor = Motor.C;
+	private static final NXTRegulatedMotor rightMotor = Motor.A;
 	private final DifferentialPilot pilot;
 	
 	private float kickSpeed;
@@ -40,19 +40,9 @@ public class AttackRobot extends Robot {
 	}
 
 	protected void moveTo(int heading, int distance) {
-		int angle;
-		
-		if (heading >= 0 && heading < 180) {
-			angle = heading;
-		} else if (heading >= 180 && heading < 360) {
-			angle = heading - 360;
-		} else {
-			angle = 0;
-			System.out.println("Bad heading value.");
-		}
-		
-		System.out.println("Rotating " + angle + " degrees.");
-		pilot.rotate(-angle, true);
+				
+		System.out.println("Rotating " + heading + " degrees.");
+		pilot.rotate(heading, true);
 		//while (pilot.isMoving() && !interrupted);
 		pilot.travel(distance * 10, true);
 		//if (interrupted) stop();
@@ -97,18 +87,7 @@ public class AttackRobot extends Robot {
 
 	@Override
 	void rotate(int heading) {
-		int angle;
-		
-		if (heading >= 0 && heading < 180) {
-			angle = heading;
-		} else if (heading >= 180 && heading < 360) {
-			angle = heading - 360;
-		} else {
-			angle = 0;
-			System.out.println("Bad heading value.");
-		}
-		
-		pilot.rotate(angle, true);
+		pilot.rotate(heading, true);
 	}
 
 	@Override
