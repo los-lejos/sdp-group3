4a95b91f6edc52945aceaa98bf4b6660fcf4daa2
Craig Wilkinson <git@craigwilkinson.co.uk>
2014-02-26 13:13:04 +0000

    Changed ToBallAction to only be possible if we are nearly facing the ball so the robot won't try and move until it is facing the ball.

--- Dice/src/dice/strategy/IssuedAction.java
+++ Dice/src/dice/strategy/IssuedAction.java
@@ -11,17 +11,14 @@ public class IssuedAction {
 	public IssuedAction() {
 		callback = new RobotCommunicationCallback() {
 			public void onError() {
-				Log.logInfo("Action error message recieved");
 				completed = true;
 			}
 			
 			public void onTimeout() {
-				Log.logInfo("Action timeout message recieved");
 				completed = true;
 			}
 			
 			public void onDone() {
-				Log.logInfo("Action done message recieved");
 				completed = true;
 			}
 		};
