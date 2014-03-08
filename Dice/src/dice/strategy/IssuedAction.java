2ba2cb522718fd8c0948e9c8d19c5d4e496e48cf
Joris S. Urbaitis <j.s.urbaitis@gmail.com>
2014-03-08 13:30:45 +0000

    Actions will now time out after a few seconds

--- Dice/src/dice/strategy/IssuedAction.java
+++ Dice/src/dice/strategy/IssuedAction.java
@@ -1,23 +1,34 @@
 package dice.strategy;
 
+import dice.Log;
 import dice.communication.RobotCommunicationCallback;
 
 public class IssuedAction {
+	
+	// After the timeout the action will be considered to be completed
+	private final int TIMEOUT = 5000;
+	
 	private RobotCommunicationCallback callback;
 	
 	private boolean completed = false;
+	private long issueTime;
 	
 	public IssuedAction() {
+		this.issueTime = System.currentTimeMillis();
+		
 		callback = new RobotCommunicationCallback() {
 			public void onError() {
+				Log.logInfo("Action completion error");
 				completed = true;
 			}
 			
 			public void onTimeout() {
+				Log.logInfo("Action timed out");
 				completed = true;
 			}
 			
 			public void onDone() {
+				Log.logInfo("Action completed");
 				completed = true;
 			}
 		};
@@ -28,6 +39,12 @@ public class IssuedAction {
 	}
 	
 	public boolean isCompleted() {
+		long currentTime = System.currentTimeMillis();
+		
+		if(Math.abs(currentTime - issueTime) > TIMEOUT) {
+			this.callback.onTimeout();
+		}
+		
 		return completed;
 	}
 }
