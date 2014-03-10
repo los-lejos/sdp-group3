ee2c949dda0afa850f15a526a9e41377545b2bd5
Joris S. Urbaitis <j.s.urbaitis@gmail.com>
2014-03-10 20:37:32 +0000

    Making the attacker's kicker open up more

--- Robot/src/robot/attacker/AttackKickerController.java
+++ Robot/src/robot/attacker/AttackKickerController.java
@@ -8,10 +8,10 @@ public class AttackKickerController extends KickerController {
 
 	private final NXTMotor motor;
 
-	private final int DELAY_OPEN = 80;
-	private final int DELAY_CLOSE = 120;
+	private final int DELAY_OPEN = 90;
+	private final int DELAY_CLOSE = 140;
 	private final int DELAY_KICK = 120;
-	private final int DELAY_KICK_CLOSE = 40;
+	private final int DELAY_KICK_CLOSE = 30;
 	
 	private final int DEFAULT_POWER = 100;
 	private final int GRAB_POWER = 50;
